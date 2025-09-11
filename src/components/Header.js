3ee584a522236a0a12548066a73e4824f0d1d981
Sejal1598 <sejalsingh1998@gmail.com>
2025-09-11 20:04:43 +0530

    complete netflix gpt

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,6 +61,10 @@ const Header = () => {
             unsubscribe()
         })
     }, [])
+
+
+
+
     return (
         <div className='absolute w-screen z-10 logo-class px-8 py-2 bg-gradient-to-b from-black flex justify-between flex-col md:flex-row  md:justify-between bg-black sm:bg-blue-900 md:bg-green-900'>
 
